6b79c1bbe7982c0e9c683811bf7822608bedf86a
Anesbeghou <abouanesmoufid@gmail.com>
2026-06-16 17:44:54 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'dakani-pos';
+
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './database.js',
+    './script.js',
+    'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;900&family=Inter:wght@300;400;600;700&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+];
+
+// التثبيت
+self.addEventListener('install', event => {
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+// التفعيل
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+// استراتيجيتك الذكية: Network First الديناميكية التلقائية
+self.addEventListener('fetch', event => {
+
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, responseClone);
+                    });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
