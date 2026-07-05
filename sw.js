afd1f9adfcc4e0be6ad3d30710fd521790e770f2
Anesbeghou <abouanesmoufid@gmail.com>
2026-07-05 16:23:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 const CACHE_NAME = 'dakani-pos';
-ي
+
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './license.js',
     './database.js',
+    './backup.js',
     './script.js',
     'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;900&family=Inter:wght@300;400;600;700&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
